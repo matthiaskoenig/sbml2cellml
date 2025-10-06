616665e88eaa0bb98b4392c56e02ed84e5395604
Matthias Koenig <konigmatt@googlemail.com>
2025-10-06 19:10:35 +0200

    working on release

---
+++ README.md
@@ -0,0 +1,12 @@
+# sbml2cellml - SBML to CellML language converter
+
+*Mission Statement*: To empower scientific research and facilitate model interoperability by providing a seamless, efficient tool that converts SBML (Systems Biology Markup Language) models into CellML, enabling broader accessibility and collaboration in computational biology and systems biology.
+
+## Installation
+
+
+## Test Suite
+
+
+
+© 2025 Matthias König
--- src/sbml2cellml/cellml2sbml.py
+++ src/sbml2cellml/cellml2sbml.py
@@ -355,23 +355,7 @@ def validate_cellml(model: libcellml.Model) -> str:
     return 0
 
 
-if __name__ == "__main__":
-    from sbmlutils.converters.cellml.cellml_simulator import run_cellml_timecourse
 
-    # converted models
-    model_names = [
-        "glimepiride_kidney",
-        "glimepiride_liver",
-        "glimepiride_intestine",
-        "glimepiride_body",
-        "glimepiride_body_flat",
-    ]
-    for name in model_names:
-        sbml_path = Path(__file__).parent / "models" / f"{name}.xml"
-        cellml_path = sbml_path.parent / f"{sbml_path.stem}.cellml"
-        model: libcellml.Model = convert_sbml2cellml(sbml_path=sbml_path)
-        write_model_to_file(model=model, cellml_path=cellml_path)
-        run_cellml_timecourse(cellml_path)
 
 
 
