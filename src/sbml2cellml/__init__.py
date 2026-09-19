e80ae98a0b6a6efce61e9d6025b8989571b5c13f
Matthias Koenig <konigmatt@googlemail.com>
2026-09-19 18:04:17 +0200

    Bump version: 0.4.0 to 0.4.1

--- src/sbml2cellml/__init__.py
+++ src/sbml2cellml/__init__.py
@@ -9,7 +9,7 @@ from sbml2cellml.sbml2cellml import convert_sbml2cellml
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Matthias Koenig"
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 program_name: str = "sbml2cellml"
 
